23e137e968be336c263e1e8c2b1aa12e8978ab4f
Prathipati Yadu Vamsi Venkata Sai <136546108+Ayushsrinivas7@users.noreply.github.com>
2025-02-07 03:49:20 +0530

    Update code_for_normal_classifer_built_from_scratch.py

--- code_for_normal_classifer_built_from_scratch.py
+++ code_for_normal_classifer_built_from_scratch.py
@@ -1,11 +1,3 @@
-# Copyright (c) Sebastian Raschka under Apache License 2.0 (see LICENSE.txt).
-# Source for "Build a Large Language Model From Scratch"
-#   - https://www.manning.com/books/build-a-large-language-model-from-scratch
-# Code: https://github.com/rasbt/LLMs-from-scratch
-#
-# This file collects all the relevant code that we covered thus far
-# throughout Chapters 2-6.
-# This file can be run as a standalone script.
 
 import os
 from pathlib import Path
@@ -21,9 +13,6 @@ import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 
 
-#####################################
-# Chapter 2
-#####################################
 
 
 class GPTDatasetV1(Dataset):
@@ -63,9 +52,7 @@ def create_dataloader_v1(txt, batch_size=4, max_length=256,
     return dataloader
 
 
-#####################################
-# Chapter 3
-#####################################
+
 class MultiHeadAttention(nn.Module):
     def __init__(self, d_in, d_out, context_length, dropout, num_heads, qkv_bias=False):
         super().__init__()
@@ -122,9 +109,7 @@ class MultiHeadAttention(nn.Module):
         return context_vec
 
 
-#####################################
-# Chapter 4
-#####################################
+
 class LayerNorm(nn.Module):
     def __init__(self, emb_dim):
         super().__init__()
@@ -247,9 +232,7 @@ def generate_text_simple(model, idx, max_new_tokens, context_size):
     return idx
 
 
-#####################################
-# Chapter 5
-#####################################
+
 def assign(left, right):
     if left.shape != right.shape:
         raise ValueError(f"Shape mismatch. Left: {left.shape}, Right: {right.shape}")
@@ -355,10 +338,6 @@ def evaluate_model(model, train_loader, val_loader, device, eval_iter):
     model.train()
     return train_loss, val_loss
 
-
-#####################################
-# Chapter 6
-#####################################
 
 
 def download_and_unzip_spam_data(url, zip_path, extracted_path, data_file_path):
